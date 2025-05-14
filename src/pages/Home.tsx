0efcfa8d2729eea136be984e99c88b9d305e3a4d
05michelleC <michellecamelo50@gmail.com>
2025-05-14 15:01:18 -0500

    Cambios home y suscribe

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import iglesia from "../assets/img/iglesia.jpg";
-import { Suscribe } from "./Suscribe"; 
+import { Suscribe } from "./Suscribe";
 
 export const Home = () => {
   return (
@@ -33,33 +33,31 @@ export const Home = () => {
           </aside>
         </section>
       </main>
-      <article className="h-screen content-center bg-slate-300 overflow-hidden bg-no-repeat lg:h-screen">
-        <section className="relative py-16 px-4 md:px-8 max-w-6xl mx-auto">
-          <div className="flex justify-between flex-row relative bg-indigo-100 rounded-3xl p-8 md:p-12 overflow-hidden">
+      <article className="flex h-screen justify-center items-center content-center bg-slate-300 overflow-hidden bg-no-repeat lg:h-screen">
+        <div className="py-16 flex h-[90%] px-4 w-[90%] flex-row relative content-center items-center bg-green-200 bg-opacity-35 rounded-3xl p-8 md:p-12 overflow-hidden">
 
-            <div className="relative z-10 w-[30rem] bg-slate-300 rounded-3xl p-8 md:p-10 shadow-lg">
-              <h2 className="text-2xl md:text-3xl font-bold mb-4 text-gray-800">Para la comunidad de Fusagasugá</h2>
+          <div className="flex flex-col items-start content-center justify-center relative z-10 w-[35rem] h-[90%] bg-slate-300 rounded-3xl p-8 md:p-10 shadow-lg left-10">
+            <h2 className="text-3xl md:text-4xl font-bold mb-4 text-gray-800">Para la comunidad de Fusagasugá</h2>
 
-              <p className="text-sm md:text-base text-gray-700 mb-6">
-                Nos especializamos en el diseño y desarrollo de estrategias virtuales innovadoras, enfocadas en ayudar
-                a empresas, artistas y proyectos a alcanzar sus metas con éxito. Creamos soluciones digitales personalizadas
-                que impulsan el crecimiento y la visibilidad en el entorno virtual, garantizando resultados efectivos y
-                sostenibles.
-              </p>
+            <p className="text-sm md:text-base text-gray-700 mb-6">
+              Nos especializamos en el diseño y desarrollo de estrategias virtuales innovadoras, enfocadas en ayudar
+              a empresas, artistas y proyectos a alcanzar sus metas con éxito. Creamos soluciones digitales personalizadas
+              que impulsan el crecimiento y la visibilidad en el entorno virtual, garantizando resultados efectivos y
+              sostenibles.
+            </p>
 
-              <button className="bg-gray-800 hover:bg-gray-700 text-white text-sm py-2 px-4 rounded-md transition duration-300">
-                Leer más
-              </button>
-            </div>
-
-            <aside className="absolute top-7 left-[30rem] w-[35rem] h-[27rem] rounded-3xl bg-slate-400 overflow-hidden md:block">
-              <img src="" alt="Aquí va la imagen" />
-            </aside>
+            <button className="bg-gray-800 hover:bg-gray-700 text-white text-sm py-4 px-6 rounded-md transition duration-300">
+              Leer más
+            </button>
           </div>
-        </section>
+
+          <aside className="absolute top-7 left-[30rem] w-[61%] h-[90%] shadow-lg rounded-3xl bg-slate-400 overflow-hidden md:block">
+            <img src="" alt="Aquí va la imagen" />
+          </aside>
+        </div>
       </article>
 
-      <Suscribe /> 
+      <Suscribe />
     </>
   );
 };
--- src/pages/Suscribe.tsx
+++ src/pages/Suscribe.tsx
@@ -3,29 +3,29 @@ export const Suscribe = () => {
     <>
       <main className="flex justify-center items-center h-screen w-screen bg-gray-100">
         {/* Contenedor*/}
-        <section className="bg-white p-10 rounded-lg shadow-xl w-full max-w-4xl">
+        <section className="bg-white p-10 rounded-lg shadow-xl max-w-4xl">
           <article className="flex flex-col justify-between items-center space-y-8"> 
-            <header className="mb-6 w-full">
+            <header className="mb-6">
               <h1 className="text-4xl font-bold text-center text-gray-800">
                 Suscribirse
               </h1>
             </header>
-            <div className="mb-8 w-full">
+            <div className="mb-8">
               <p className="text-center text-gray-600">
                 Suscríbete a nuestro boletín para mantenerte informado sobre las
                 últimas novedades, servicios y oportunidades que Fusga tiene para ti.
               </p>
             </div>
-            <form className="flex flex-col w-full gap-6">
+            <form className="flex flex-col gap-6">
               <input
                 type="email"
                 id="email"
                 placeholder="ejemplo@correo.com"
-                className="border border-gray-300 p-3 rounded w-full focus:outline-none focus:ring-2 focus:ring-violet-400"
+                className="border border-gray-300 p-3 rounded focus:outline-none focus:ring-2 focus:ring-violet-400"
               />
               <button
                 type="submit"
-                className="hover:bg-violet-600 bg-violet-400 text-white rounded-full h-12 w-full mt-6"
+                className="hover:bg-violet-600 bg-violet-400 text-white rounded-full h-12 mt-6"
               >
                 Suscribirse
               </button>

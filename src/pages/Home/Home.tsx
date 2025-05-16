3c76ee84581b352ad00186a1c1b784fbae7722ec
Mateusaxv <ximenavanegas88@gmail.com>
2025-05-16 13:23:43 -0500

    home doble borrado

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import { Hero } from "./components/Hero"
 import { ComunityH } from "./components/Comunity";
 import { Suscribe } from "./components/Suscribe";
-import { Faq } from "./components/faq";
+import { Faq } from "./components/Faq";
 
 export const Home = () => {
   return (
